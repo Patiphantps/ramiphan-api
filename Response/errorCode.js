dcee2f1b977f5977ca38ea0c9d62052aba0e6f84
Blue <ramida470@gmail.com>
2025-08-29 22:33:11 +0700

    format

--- Response/errorCode.js
+++ Response/errorCode.js
@@ -1,36 +1,38 @@
 const success = {
-    returnCode: "00000",
-    returnDescTH: "ทำรายการสำเร็จ",
-    returnDesc: "Success"
-}
+  returnCode: "00000",
+  returnDescTH: "ทำรายการสำเร็จ",
+  returnDesc: "Success",
+};
+
 const invalid_data_err = {
-    returnCode: "10001",
-    returnDescTH: "ข้อมูลไม่ครบถ้วน กรุณากรอกใหม่อีกครั้ง",
-    returnDesc: "Invalid Data"
-}
+  returnCode: "10001",
+  returnDescTH: "ข้อมูลไม่ครบถ้วน กรุณากรอกใหม่อีกครั้ง",
+  returnDesc: "Invalid Data",
+};
 
 const malformed_JSON = {
-    returnCode: "10002",
-    returnDesc: "Malformed JSON",
-    returnDescTH: "ข้อมูลไม่ครบถ้วน กรุณากรอกใหม่อีกครั้ง"
-}
+  returnCode: "10002",
+  returnDesc: "Malformed JSON",
+  returnDescTH: "ข้อมูลไม่ครบถ้วน กรุณากรอกใหม่อีกครั้ง",
+};
 
 const database_err = {
-    returnCode: "10003",
-    returnDescTH: "ขออภัยในความไม่สะดวก ขณะนี้ระบบขัดข้อง กรุณาลองใหม่ภายหลังภายหลัง",
-    returnDesc: "Database error"
-}
+  returnCode: "10003",
+  returnDescTH:
+    "ขออภัยในความไม่สะดวก ขณะนี้ระบบขัดข้อง กรุณาลองใหม่ภายหลังภายหลัง",
+  returnDesc: "Database error",
+};
 
 const internal_err = {
-    returnCode: "10004",
-    returnDescTH: "ขออภัยในความไม่สะดวก ขณะนี้ระบบขัดข้อง กรุณาลองใหม่ภายหลัง",
-    returnDesc: "Internal error"
-}
+  returnCode: "10004",
+  returnDescTH: "ขออภัยในความไม่สะดวก ขณะนี้ระบบขัดข้อง กรุณาลองใหม่ภายหลัง",
+  returnDesc: "Internal error",
+};
 
 export default {
-    success,
-    invalid_data_err,
-    malformed_JSON,
-    database_err,
-    internal_err
-}
+  success,
+  invalid_data_err,
+  malformed_JSON,
+  database_err,
+  internal_err,
+};
--- Response/gatewayException.js
+++ Response/gatewayException.js
@@ -1,31 +1,31 @@
 class DomainError extends Error {
-    constructor(message) {
-        super(message)
-        this.name = this.constructor.name
-        Error.captureStackTrace(this, this.constructor)
-    }
+  constructor(message) {
+    super(message);
+    this.name = this.constructor.name;
+    Error.captureStackTrace(this, this.constructor);
+  }
 }
 
 class gatewayException extends DomainError {
-    constructor(code, msg, status) {
-        super(code)
-        this.data = { code, msg, status }
+  constructor(code, msg, status) {
+    super(code);
+    this.data = { code, msg, status };
+  }
+  toResponse() {
+    let respModel = {};
+    respModel = { ...this.data.code };
+    if (this.data.msg !== "") {
+      respModel.errorCause = this.data.msg;
     }
-    toResponse() {
-        let respModel = {}
-        respModel = { ...this.data.code }
-        if (this.data.msg !== '') {
-            respModel.errorCause = this.data.msg
-        }
-        return {
-            statusCode: this.data.status,
-            body: respModel,
-            headers: {
-                'Content-Type': 'application/json',
-                'Access-Control-Allow-Origin': '*'
-            }
-        }
-    }
+    return {
+      statusCode: this.data.status,
+      body: respModel,
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+      },
+    };
+  }
 }
 
-export default gatewayException
+export default gatewayException;
